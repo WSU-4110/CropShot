89fa9a06521089ab6d8e98b36faf17a81fa34673
Yash Kondapalli <ef1739@wayne.edu>
2019-10-30 23:44:48 -0400

    Very large commit. Got in the zone and forgot to commit frequntly. THis code contains the new manualcrop xml format file as well as half of the work on the second activity java code.

--- app/src/main/java/com/example/cropshot/ManualCrop.java
+++ app/src/main/java/com/example/cropshot/ManualCrop.java
@@ -1,14 +1,68 @@
 package com.example.cropshot;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class ManualCrop extends AppCompatActivity {
+    ImageButton browser,btReset;
+    ImageView imageView;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manual_crop);
+
+        browser = findViewById(R.id.b_browser);
+        btReset = findViewById(R.id.bt_reset);
+        imageView = findViewById(R.id.image_view);
+
+        browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.startPickImageActivity(ManualCrop.this);
+            }
+        });
+
+        btReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageBitmap(null);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE
+        && resultCode == Activity.RESULT_OK){
+            Uri imageuri = CropImage.getPickImageResultUri(this,data);
+            if (CropImage.isReadExternalStoragePermissionsRequired(this,imageuri)){
+                uri = imageuri;
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}
+                ,0);
+            }else{
+                startCrop(imageuri);
+            }
+        }
+    }
+
+    private void startCrop(Uri imageuri) {
+        CropImage.activity(imageuri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setMultiTouchEnabled(true)
+                .start(this);
     }
 }
