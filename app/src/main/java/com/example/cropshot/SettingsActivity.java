f5b9457c0a3cb458c52ea61fdd551fa5522f048e
Yash Kondapalli <ef1739@wayne.edu>
2019-11-22 01:12:43 -0500

    Settings activity has dark mode

--- app/src/main/java/com/example/cropshot/SettingsActivity.java
+++ app/src/main/java/com/example/cropshot/SettingsActivity.java
@@ -1,22 +1,33 @@
 package com.example.cropshot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.app.AppCompatDialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Button button;
     private Button about;
+    private Switch darkswitch;
+    private Switch MLswitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+                setTheme(R.style.darktheme);
+        }
+        else setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         button = (Button) findViewById(R.id.back);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +43,24 @@ public class SettingsActivity extends AppCompatActivity {
                 openAbout();
             }
         });
+
+        darkswitch = findViewById(R.id.darkswitch);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+                darkswitch.setChecked(true);
+        }
+        darkswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
     }
 
     public void openGOBACK(){
@@ -42,5 +71,11 @@ public class SettingsActivity extends AppCompatActivity {
     public void openAbout(){
         AboutBox aboutBox = new AboutBox();
         aboutBox.show(getSupportFragmentManager(),"about box");
+    }
+
+    public void restartApp(){
+        Intent i = new Intent(getApplicationContext(),SettingsActivity.class);
+        startActivity(i);
+        finish();
     }
 }
