6eca993e9a11eeb7708c57f6b287cf3f45a582c4
Shafournee <31718336+Shafournee@users.noreply.github.com>
2019-11-23 23:12:34 -0500

    Created a settings singleton to hold onto settings values for activities to reference

---
+++ app/src/main/java/com/example/cropshot/SettingsSingleton.java
@@ -0,0 +1,40 @@
+package com.example.cropshot;
+
+public class SettingsSingleton {
+
+    private static SettingsSingleton INSTANCE = null;
+
+    private SettingsSingleton() {};
+    private boolean isDarkMode = false;
+    private boolean useML = false;
+
+    // If the class doesn't already exist, create a new instance of it
+    // otherwise, return the currently existing instance.
+    public static SettingsSingleton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SettingsSingleton();
+        }
+        return(INSTANCE);
+    }
+
+    public void setDarkMode(boolean set)
+    {
+        isDarkMode = set;
+    }
+
+    public boolean getDarkMode()
+    {
+        return isDarkMode;
+    }
+
+    public void setUseML(boolean set)
+    {
+        useML = set;
+    }
+
+    public boolean getUseML()
+    {
+        return useML;
+    }
+
+}
