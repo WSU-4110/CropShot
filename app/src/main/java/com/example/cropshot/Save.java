bd2a9c5b4f24973c737129c5f6513de1dfd47beb
Sabih Ijaz <gg5395@wayne.edu>
2019-12-01 17:11:54 -0500

    finished rewrite of savenew

--- app/src/main/java/com/example/cropshot/Save.java
+++ app/src/main/java/com/example/cropshot/Save.java
@@ -22,40 +22,8 @@ public class Save {
 
 
 
-    public void saveAsNew2(Bitmap finalBitmap) {
-        File mydir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"saved_images");
-        mydir.mkdirs();
-        FileOutputStream fostream = null;
-        try {
-            //fostream = new FileOutputStream(createFile());//
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG,100,fostream);
 
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                if (fostream != null)
-                    fostream.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
-    /*@NonNull
-    private File createFile() {
-        File directory = this.context.getDir("saved_images",Context.MODE_PRIVATE);
-        if (!directory.exists() && !directory.mkdirs())
-            Log.e("Saver","can't make directory");
-        Random generator = new Random();
-        int n = 10000;
-        n = generator.nextInt(n);
-        String fname = "ImageCropshotters-" + n + ".jpg";
-        return new File("saved_images",fname);
-    }*/
 
     public static File mainDirectory(Context context) {
         File mainDir = new File ("/sdcard/savepics");
@@ -87,34 +55,12 @@ public class Save {
 
     }
 
-    public void saveAsNew(Bitmap finalBitmap)
-    {
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root + "sdcasaved_images");
-        myDir.mkdirs();
-        Random generator = new Random();
-        int n = 10000;
-        n = generator.nextInt(n);
-        String fname = "ImageCropShotter-" + n + ".jpg";
-        File file = new File(myDir, fname);
-        Log.i("LOAD", root + fname);
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 
     public void Overwrite(Bitmap finalBitmap)
     {
 
     }
 
-    public void SaveAs(String name, Bitmap finalBitmap)
-    {
 
-    }
 }
