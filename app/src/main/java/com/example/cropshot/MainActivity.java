ab8dd875015714c08923f020f9285e05efdb524b
Shafournee <31718336+Shafournee@users.noreply.github.com>
2019-12-01 22:40:18 -0500

    Fixed a merge conflict issue

--- app/src/main/java/com/example/cropshot/MainActivity.java
+++ app/src/main/java/com/example/cropshot/MainActivity.java
@@ -205,7 +205,8 @@ public class MainActivity extends AppCompatActivity {
 
             Save save = new Save();
 
-            save.saveAsNew(croppedMap);
+            File saveFile = Save.mainDirectory(MainActivity.this);
+            save.saver(croppedMap, saveFile);
 
             // First add 1 to the file position
             filePos++;
