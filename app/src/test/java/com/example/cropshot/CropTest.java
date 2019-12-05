9d4e163976437ecf9d0a6755bf75cbf4d67d0f91
Yash Kondapalli <ef1739@wayne.edu>
2019-12-05 15:37:23 -0500

    CropTest.java

---
+++ app/src/test/java/com/example/cropshot/CropTest.java
@@ -0,0 +1,20 @@
+package com.example.cropshot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CropTest {
+
+    @Test
+    public void checkColor() {
+    }
+
+    @Test
+    public void findBorder() {
+    }
+
+    @Test
+    public void solidRow() {
+    }
+}
