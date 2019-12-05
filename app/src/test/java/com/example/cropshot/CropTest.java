8bb7b0c30f63dcc3d50b530423032f900a3ae236
Yash Kondapalli <ef1739@wayne.edu>
2019-12-05 16:37:29 -0500

    CropTest unit tests

--- app/src/test/java/com/example/cropshot/CropTest.java
+++ app/src/test/java/com/example/cropshot/CropTest.java
@@ -1,5 +1,7 @@
 package com.example.cropshot;
 
+import android.graphics.Bitmap;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,14 +10,29 @@ public class CropTest {
 
     @Test
     public void checkColor() {
+        Crop CropTest = new Crop();
 
+        assertTrue(CropTest.CheckColor(20,25));
+        assertTrue(CropTest.CheckColor(25,25));
+        assertFalse(CropTest.CheckColor(100,25));
     }
 
     @Test
     public void findBorder() {
+
+        Crop CropTest = new Crop();
+        Bitmap bit = mock(Bitmap.class);
+        MainActivity MATEST = mock (MainActivity.class);
+        assertTrue(CropTest.FindBorder(MATEST.DIR.TOP,bit));
+
     }
 
     @Test
     public void solidRow() {
+
+        Crop CropTest = new Crop();
+        Bitmap bit = mock(Bitmap.class);
+
+        assertTrue(CropTest.SolidRow(bit));
     }
 }
